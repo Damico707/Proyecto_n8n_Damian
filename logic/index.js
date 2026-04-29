1a9ff56445a27577d8f2967bfd31796468895b92
Damico707 <damian707rangel@gmail.com>
2026-04-29 06:50:02 -0500

    build: :fire: html and css forms part
    Construi el formulario en html y css mientras trabajo en el flujo en n8n

---
+++ logic/index.js
@@ -0,0 +1,6 @@
+const nombre= document.getElementById ("name");
+const id= document.getElementById("id")
+const correo = document.getElementById("mail")
+const curso = document.getElementById("course")
+const motivo = document.getElementById("reason")
+
