29a5249db97ad7aef76dc2c2eaf2c73f8698ecb9
Damico707 <damian707rangel@gmail.com>
2026-04-30 20:59:24 -0500

    test: :zap: Archivos de json y arreglos minimos

--- logic/index.js
+++
@@ -1,6 +0,0 @@
-const nombre= document.getElementById ("name");
-const id= document.getElementById("id")
-const correo = document.getElementById("mail")
-const curso = document.getElementById("course")
-const motivo = document.getElementById("reason")
-
